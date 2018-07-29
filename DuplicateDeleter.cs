3e8721f737f77d879e8fc1f92d4898bab4dd978b
Carolynn Vansant <carolynnmarie@gmail.com>
2018-07-28 20:49:18 -0400

    added more methods

--- DuplicateDeleter.cs
+++ DuplicateDeleter.cs
@@ -34,7 +34,37 @@ namespace C_Sharp_DuplicateDeleter{
             }
             return list.ToArray();
         }
+        public int getMostCommonItem(){
+            int temp = 0;
+            int most = 0;
+            foreach(var item in randomArray){
+                if(countDuplicates(item,randomArray)>temp){
+                    temp = countDuplicates(item,randomArray);
+                    most = item;
+                }
+            }
+            return  most;
+        }
+        public int[] deleteMultiplesOfN(int multiple){
+            List<int> list = new List<int>();
+            foreach(var x in randomArray){
+                if(x%multiple != 0 || x == 0){
+                    list.Add(x);
+                }
+            }
+            int[] array = list.ToArray();
+            return array; 
+        }
 
+        public static int[] mergeArrays(int [] objectArray, int[] objectArrayToAdd){
+            int [] merged = new int[objectArray.Length + objectArrayToAdd.Length];
+            objectArray.CopyTo(merged,0);
+            objectArrayToAdd.CopyTo(merged,objectArray.Length);
+            return merged;
+        }
+
+        }
+
+        
     }
     
-}
--- Program.cs
+++ Program.cs
@@ -6,16 +6,22 @@ namespace C_Sharp_DuplicateDeleter
     {
         static void Main(string[] args)
         {
-            RandomNumberGenerator generator = new RandomNumberGenerator();
-            int [] randomArray = generator.randomNumberArray(15, 8);
-            Console.WriteLine(String.Join(" ",randomArray));
+            // RandomNumberGenerator generator = new RandomNumberGenerator();
+            // int [] randomArray = generator.randomNumberArray(15, 8);
+            // Console.WriteLine(String.Join(" ",randomArray));
 
-            DuplicateDeleter deleter = new DuplicateDeleter(randomArray);
-            int[] deleted = deleter.removeDuplicates(3);
-            Console.WriteLine(String.Join(" ",deleted));
+            // DuplicateDeleter deleter = new DuplicateDeleter(randomArray);
+            // int[] deleted = deleter.removeDuplicates(3);
+            // Console.WriteLine(String.Join(" ",deleted));
             
-            int[] exactDeleted = deleter.removeExactDuplications(3);
-            Console.WriteLine(String.Join(" ",exactDeleted));
+            // int[] exactDeleted = deleter.removeExactDuplications(3);
+            // Console.WriteLine(String.Join(" ",exactDeleted));
+
+            // Console.WriteLine(String.Join(" ", deleter.deleteMultiplesOfN(2)));
+            int [] array1 = {1,2,3,4};
+            int [] array2 = {5,6,7,8};
+            int [] merged = DuplicateDeleter.mergeArrays(array1,array2);
+            Console.WriteLine(String.Join(" ", merged));
         }
     }
 }
